550e452cfff56b5ca53ae70250b4964e131879e7
Paulo Sergio <paulo_sergio2006@hotmail.com.br>
2021-05-05 16:58:38 -0300

    organizaçao

--- backend/src/serve.js
+++ backend/src/serve.js
@@ -6,14 +6,14 @@ const server = express();
 //definindo a porta e o hostnao
 const port = 3000;
 const hostname = "localhost";
- 
-//chamando as classes das rotas
-const pessoa = require('./src/routes/pessoa-rotas');
-const agenda = require('./src/routes/agenda-rotas');
-const uniSaude = require('./src/routes/unisaude-rotas');
 
-server.use(express.urlencoded({extended: true})); 
-server.use(express.json());   
+//chamando as classes das rotas
+const pessoa = require('./routes/pessoa-rotas');
+const agenda = require('./routes/agenda-rotas');
+const uniSaude = require('./routes/unisaude-rotas');
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
 
 //passando as rotas para as classes responsaveis
@@ -21,19 +21,20 @@ server.use('/api/pessoa/', pessoa);
 server.use('/api/agenda/', agenda);
 server.use('/api/unisaude/', uniSaude);
 
-mongoose.connect('mongodb://root:paulo123@localhost:27017/devwebII?authSource=admin', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://root:paulo123@localhost:27017/devwebII?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'erro ao conectar no mongodb'));
-db.once('open',function () {
+db.on('error', console.error.bind(console, 'erro ao conectar no mongodb'));
+db.once('open', function () {
     console.log('sucesso ao conectar no mongodb');
 });
 
 server.get('/', function (req, res) {
     res.json({
-         status: "ok", 
-         message: "Servidor rodando" });
+        status: "ok",
+        message: "Servidor rodando"
+    });
 });
 
 server.listen(port, hostname, function () {
